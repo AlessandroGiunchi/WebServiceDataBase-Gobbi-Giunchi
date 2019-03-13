f5f62c15fcaf4f93cce36d7843d960d2d673bf3d
Enrico Gobbi <enrico.gobbi.itiscesena@gmail.com>
2019-03-13 17:46:12 +0100

    Conversione da query JSON a query SQL

---
+++ Client/ClientWebServices_Giunchi_Gobbi/ClientWebServices_Giunchi_Gobbi/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+using System.Threading;
+
+namespace ClientWebServices_Giunchi_Gobbi
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string mycontent = "";
+        static string titoli;
+        static string[] splittato = new string[10];
+        string url = "http://10.13.100.5/gobbi/WebServicesLibri-Gobbi-Giunchi/Server/?funzione=";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        async static Task GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        mycontent = await content.ReadAsStringAsync();
+
+                        bool find = true;
+                        int start = 0;
+                        int end = 0;
+                        int i = 0;
+
+                        while (find == true)
+                        {
+                            if (mycontent.Substring(start).Contains("data"))
+                            {
+                                start = mycontent.IndexOf("data", start);
+                                end = mycontent.IndexOf("}", start);
+                                titoli = mycontent.Substring(start + 7, end - start - 8);
+                                splittato = titoli.Split(',');
+                                start++;
+                                i++;
+                            }
+                            else
+                            {
+                                find = false;
+                            }
+                        }
+                        //MessageBox.Show(mycontent);
+                    }
+                }
+            }
+        }
+
+        private void btn_pulisci_Click(object sender, RoutedEventArgs e)
+        {
+            lst_libri.Items.Clear();
+        }
+
+        private async void btn_visualizza_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = GetRequest(url + "0");
+            await task;
+
+            /*foreach (string tmp in splittato)
+            {
+                str = tmp.Trim('"');
+                lst_libri.Items.Add(str);
+            }*/
+
+            Visualizza(splittato);
+        }
+
+        private async void btn_visualizza_elenco_libri_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = GetRequest(url + "2");
+            await task;
+
+            /*foreach (string tmp in splittato)
+            {
+                str = tmp.Trim('"');
+                lst_libri.Items.Add(str);
+            }*/
+
+            Visualizza(splittato);
+
+            //MessageBox.Show(mycontent);
+        }
+
+        private async void btn_elenco_tra_date_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = GetRequest(url + "3" + "&data1=" + txt_data1.Text + "&data2=" + txt_data2.Text);
+            await task;
+
+            /*foreach (string tmp in splittato)
+            {
+                str = tmp.Trim('"');
+                lst_libri.Items.Add(str);
+            }*/
+
+            //MessageBox.Show(mycontent);
+
+            Visualizza(splittato);
+        }
+
+        private async void btn_codice_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = GetRequest(url + "4" + "&codice=" + txt_codice.Text);
+            await task;
+
+            //Visualizza(splittato);
+            MessageBox.Show(mycontent);
+        }
+
+        public void Visualizza(string[] str)
+        {
+            string stamp;
+            foreach (string tmp in str)
+            {
+                stamp = tmp.Trim('"');
+                lst_libri.Items.Add(stamp);
+            }
+        }
+
+        private async void btn_ultimiArrivi_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = GetRequest(url + "1" /*+ "&reparto=" + cmb_reparto.SelectedItem.ToString().ToLower()*/);
+            await task;
+
+            /* (string tmp in splittato)
+            {
+                str = tmp.Trim('"');
+                lst_libri.Items.Add(str);
+            }*/
+
+            Visualizza(splittato);
+        }
+    }
+}
---
+++ Server/index.php
@@ -0,0 +1,180 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "libridb";
+
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//process client request (via URL)
+header ("Content-Type_application/json");
+
+//Verifica che sia stata passata un numero di funzione da eseguire
+// if(empty($_GET['funzione']))
+// 	$funzione = null;
+// else
+$funzione = $_GET['funzione'];
+
+switch($funzione)
+{
+	case '0':
+		//Query elenco tutti libri
+		$stmt = $conn->prepare("SELECT titolo FROM libri");
+		$stmt->execute();
+		$res = $stmt->fetchAll();
+		
+		//inizializzazione variabili
+		$arr = array();
+		$i = 0;
+		
+		foreach($res as $row)
+		{
+			$arr[$i] = $row['titolo'];
+			$i = $i + 1;
+		}
+		
+		deliver_response(200,"all books", $arr);
+		break;
+		
+	case '1':
+		//inizializzazione variabili
+		$arr = array();
+		$i = 0;
+		
+		$stmt = $conn->prepare("SELECT COUNT(libri.id) 
+								FROM libri JOIN reparti on libri.reparto = reparti.id
+								JOIN categorielibri on libri.id = categorielibri.libro
+								WHERE reparti.tipo = 'Fumetti' and categorielibri.categoria = 'Ultimi arrivi'");
+		$stmt->execute();
+		$res = $stmt->fetchAll();
+
+		//var_dump($res);
+		foreach($res as $row)
+		{
+			$arr[$i] = $row['COUNT(libri.id)'];
+			$i = $i + 1;
+		}
+		
+		deliver_response(200,"fumetti ", $arr);
+		break;
+
+	case '2':
+		//inizializzazione variabili
+		$tit=array();
+		
+		$stmt = $conn->prepare("SELECT titolo 
+								FROM libri JOIN categorielibri ON libri.id = categorielibri.libro
+								JOIN categorie on categorielibri.categoria = categorie.tipo
+								WHERE sconto > 0
+								ORDER BY sconto");
+		$stmt->execute();
+		$res = $stmt->fetchAll();
+
+		//var_dump($res);
+		foreach($res as $row)
+		{
+			$arr[$i] = $row['titolo'];
+			$i = $i + 1;
+		}
+
+		deliver_response(200,"scontati", $arr);
+		break;
+
+	case '3':
+		//inizializzazione variabili
+		$arr = array();
+
+		//split date inserite in input
+		$tmpInizio = explode('/', $_GET['data1']);;
+		$tmpFine = explode('/', $_GET['data2']);
+
+		//Timestamp data inizio ricerca
+		$dataInizio = mktime(0,0,0,$tmpInizio[1],$tmpInizio[0],$tmpInizio[2]);
+
+		//Timestamp data inizio ricerca
+		$dataFine = mktime(0,0,0,$tmpFine[1],$tmpFine[0],$tmpFine[2]);
+
+		$stmt = $conn->prepare("SELECT titolo 
+								FROM libri
+								WHERE dataArch BETWEEN ".$tmpInizio[2].$tmpInizio[1].$tmpInizio[0]." AND ".$tmpFine[2],$tmpFine[1],$tmpFine[0]);
+		$stmt->execute();
+		$res = $stmt->fetchAll();
+
+		//var_dump($res);
+		foreach($res as $row)
+		{
+			$arr[$i] = $row['titolo'];
+			$i = $i + 1;
+		}
+		
+		deliver_response(200,"date    ", $arr);
+		break;
+
+	case '4':
+		//Conversione file JSON in array associativo
+		$dati = conversioneDati('../FileJSON/Libri.json');
+		$user = conversioneDati('../FileJSON/Utenti.json');
+		$carrelli = conversioneDati('../FileJSON/Carrelli.json');
+		$libriCarr = conversioneDati('../FileJSON/LibriCarrello.json');
+
+		$idCarr = $_GET['codice'];
+		$utente="";
+		$tit = array();
+		$nCopie = "";
+
+		foreach($carrelli['carrello'] as $carr)
+		{
+			if($idCarr ==  $carr["id"])
+				$utente = $carr["utente"];
+		}
+
+		foreach($libriCarr['librocarrello'] as $associazione)
+		{
+			if($associazione['carrello'] === $idCarr);
+			{
+				foreach($dati['libro'] as $book)
+				{
+					if($book['id'] === $associazione['libro'])
+						array_push($tit, array('titolo'=>$book['titolo'], 'nCopie' => $associazione['nCopie']));
+				}
+
+				//$nCopie = $associazione['nCopie'];
+			}
+		}
+
+		$arr = array();
+		array_push($arr, array('utente'=>$utente, 'libri' => $tit));
+
+		deliver_response(200,"carrello", $arr);
+		break;
+
+	default:
+		deliver_response(400,"Invalid request", NULL);
+		break;
+}
+
+//Funzione per invio della risposta al client
+function deliver_response($status, $status_message, $data)
+{
+	header("HTTP/1.1 $status $status_message");
+	
+	$response ['status']=$status;
+	$response['status_message']=$status_message;
+	$response['data']=$data;
+	
+	$json_response=json_encode($response);
+	echo $json_response;
+}
+
+//Funzione per la conversione da file JSON a array associativo
+function conversioneDati($json)
+{
+	$str = file_get_contents($json);
+	$dati = json_decode($str, true); 
+	
+	return $dati;
+}
+?>
